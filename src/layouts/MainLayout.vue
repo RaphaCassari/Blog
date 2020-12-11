34f27ada8f26bd23c7718b86d4e3235a757f9548
Raphael Cassari Benedicto <raphacassari@gmail.com>
2020-12-11 18:13:03 -0300

    bla

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,78 @@
+<template>
+  <q-layout >
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Home',
+    caption: '',
+    icon: 'account_box',
+    link: '/'
+  },
+  {
+    title: 'Posts',
+    caption: 'Lista de Postagens',
+    icon: 'list',
+    link: '/posts'
+  }
+]
+
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'MainLayout',
+  components: { EssentialLink },
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const essentialLinks = ref(linksData)
+
+    return { leftDrawerOpen, essentialLinks }
+  }
+})
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-page class="container">
+    </q-page>
+</template>
+
+<style>
+
+</style>
